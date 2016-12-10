478a24baad31253201cfcfa05e8dbb9055137812
野口　茉莉子 <marikonoguchi@noguchimariko-no-MacBook-Air.local>
2016-12-10 13:20:28 -0600

    Initial commit

---
+++ app/src/main/java/edu/mnstate/cw3967me/lab7_2/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.mnstate.cw3967me.lab7_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
